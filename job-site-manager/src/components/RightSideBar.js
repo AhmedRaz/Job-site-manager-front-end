f748545bf74f9fb9e6a9ccc1393b3d83911cf96f
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-08-22 19:35:46 -0400

    added right side bar, modified css

---
+++ job-site-manager/src/components/RightSideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RightSideBar extends React.Component {
+  render(){
+    return(
+      <div id="right-side-bar">
+        things might go here
+      </div>
+    );
+  }
+}
+
+export default RightSideBar
